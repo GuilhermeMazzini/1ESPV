235a26da1b548ed38628b1a18d2cb1604b230d5e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 19:50:20 -0300

    Adicionando arquivos iniciais

---
+++ Aula 2/script.js
@@ -0,0 +1,10 @@
+let a = 5, b = 5;
+
+console.log(a == b);   // false (igualdade)
+console.log(a === b);  // false (igualdade estrita)
+console.log(a != b);   // true (diferente)
+console.log(a !== b);  // true (diferente estrito)
+console.log(a < b);    // true (menor que)
+console.log(a > b);    // false (maior que)
+console.log(a <= b);   // true (menor ou igual)
+console.log(a >= b);   // false (maior ou igual)
